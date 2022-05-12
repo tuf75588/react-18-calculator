a78dd76a586d9707be78d81ee55c7e4f92f54491
Andrew Davis <atd285@gmail.com>
2022-05-11 21:11:14 -0400

    UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,65 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          yes
-      </header>
+    <div className='calc'>
+      <div className='calc-btn display'>0</div>
+      <div className='calc-btn'>
+        <button>AC</button>
+      </div>
+      <div className='calc-btn'>
+        <button>+/-</button>
+      </div>
+      <div className='calc-btn'>
+        <button>%</button>
+      </div>
+      <div className='calc-btn'>
+        <button>/</button>
+      </div>
+      <div className='calc-btn'>
+        <button>7</button>
+      </div>
+      <div className='calc-btn'>
+        <button>8</button>
+      </div>
+      <div className='calc-btn'>
+        <button>9</button>
+      </div>
+      <div className='calc-btn'>
+        <button>X</button>
+      </div>
+      <div className='calc-btn'>
+        <button>4</button>
+      </div>
+      <div className='calc-btn'>
+        <button>5</button>
+      </div>
+      <div className='calc-btn'>
+        <button>6</button>
+      </div>
+      <div className='calc-btn'>
+        <button>-</button>
+      </div>
+      <div className='calc-btn'>
+        <button>1</button>
+      </div>
+      <div className='calc-btn'>
+        <button>2</button>
+      </div>
+      <div className='calc-btn'>
+        <button>3</button>
+      </div>
+      <div className='calc-btn'>
+        <button>+</button>
+      </div>
+      <div className='calc-btn zero'>
+        <button>0</button>
+      </div>
+      <div className='calc-btn'>
+        <button>.</button>
+      </div>
+      <div className='calc-btn'>
+        <button>=</button>
+      </div>
     </div>
   );
 }
