e62dbbb05db6e815634954c2a447e489c2382878
Andrew Davis <atd285@gmail.com>
2022-05-14 15:56:21 -0400

    fix bug with value state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  const [value, setValue] = React.useState<any | null>(0);
+  const [value, setValue] = React.useState<any | null>(null);
   const [displayValue, setDisplay] = React.useState<any | null>('0');
   const [operator, setOperator] = React.useState<any | null>(null);
   const [waitingForOperand, setWaitingForOperand] = React.useState(false);
@@ -10,15 +10,16 @@ function App() {
     if (waitingForOperand) {
       setDisplay(String(digit));
       setWaitingForOperand(false);
+    } else {
+      setDisplay(displayValue === '0' ? String(digit) : displayValue + digit);
     }
   };
 
   const clearDisplay = () => {
     setDisplay('0');
-    setOperation('');
   };
   const clearAll = () => {
-    setValue(0);
+    setValue(null);
     setDisplay('0');
     setOperator(null);
     setWaitingForOperand(false);
@@ -38,28 +39,35 @@ function App() {
     setDisplay(String(toNum / 100));
   };
 
-  const performOperation = (operator: string) => {
-    setOperation(operator);
-    setWaitingForOperand(true);
-    const nextValue = parseFloat(displayValue);
-    const prevValue = parseFloat(value);
+  const performOperation = (nextOperator: string) => {
     const operations: any = {
       '/': (previousValue: number, nextValue: number) => previousValue / nextValue,
       '*': (previousValue: number, nextValue: number) => previousValue * nextValue,
       '+': (previousValue: number, nextValue: number) => previousValue + nextValue,
-      '=': (nextValue: number) => nextValue,
+      '=': (previousValue: number, nextValue: number) => nextValue,
     };
-    const computedValue = operations[operator](nextValue, prevValue);
+    const inputValue = parseFloat(displayValue);
+    if (value === null) {
+      setValue(inputValue);
+    } else if (operator) {
+      const currentValue = value || 0;
+      const newValue = operations[operator](currentValue, inputValue);
+      setValue(newValue);
+
+      setDisplay(String(newValue));
+    }
+    setWaitingForOperand(true);
+    setOperator(nextOperator);
   };
   return (
     <React.Fragment>
       <pre style={{ color: 'white', fontSize: '1.2rem' }}>
-        {JSON.stringify({ operation, displayValue, waitingForOperand, value }, null, 2)}
+        {JSON.stringify({ operator, displayValue, waitingForOperand, value }, null, 2)}
       </pre>
       <div className="calc">
         <div className="calc-btn display">{displayValue}</div>
         <div className="calc-btn">
-          <button onClick={() => clearDisplay()}>AC</button>
+          <button onClick={() => clearAll()}>AC</button>
         </div>
         <div className="calc-btn">
           <button onClick={() => toggleSign()}>+/-</button>
@@ -68,60 +76,60 @@ function App() {
           <button onClick={() => inputPercent()}>%</button>
         </div>
         <div className="calc-btn">
-          <button className={operation === '/' ? 'active' : ''} onClick={() => performOperation('/')}>
+          <button className={operator === '/' ? 'active' : ''} onClick={() => performOperation('/')}>
             /
           </button>
         </div>
         <div className="calc-btn">
-          <button onClick={() => addDigit('7')}>7</button>
+          <button onClick={() => inputDigit('7')}>7</button>
         </div>
         <div className="calc-btn">
-          <button onClick={() => addDigit('8')}>8</button>
+          <button onClick={() => inputDigit('8')}>8</button>
         </div>
         <div className="calc-btn">
-          <button onClick={() => addDigit('9')}>9</button>
+          <button onClick={() => inputDigit('9')}>9</button>
         </div>
         <div className="calc-btn">
-          <button className={operation === '*' ? 'active' : ''} onClick={() => performOperation('*')}>
+          <button className={operator === '*' ? 'active' : ''} onClick={() => performOperation('*')}>
             X
           </button>
         </div>
         <div className="calc-btn">
-          <button onClick={() => addDigit('4')}>4</button>
+          <button onClick={() => inputDigit('4')}>4</button>
         </div>
         <div className="calc-btn">
-          <button onClick={() => addDigit('5')}>5</button>
+          <button onClick={() => inputDigit('5')}>5</button>
         </div>
         <div className="calc-btn">
-          <button onClick={() => addDigit('6')}>6</button>
+          <button onClick={() => inputDigit('6')}>6</button>
         </div>
         <div className="calc-btn">
-          <button className={operation === '-' ? 'active' : ''} onClick={() => performOperation('-')}>
+          <button className={operator === '-' ? 'active' : ''} onClick={() => performOperation('-')}>
             -
           </button>
         </div>
         <div className="calc-btn">
-          <button onClick={() => addDigit('1')}>1</button>
+          <button onClick={() => inputDigit('1')}>1</button>
         </div>
         <div className="calc-btn">
-          <button onClick={() => addDigit('2')}>2</button>
+          <button onClick={() => inputDigit('2')}>2</button>
         </div>
         <div className="calc-btn">
-          <button onClick={() => addDigit('3')}>3</button>
+          <button onClick={() => inputDigit('3')}>3</button>
         </div>
         <div className="calc-btn">
-          <button className={operation === '+' ? 'active' : ''} onClick={() => performOperation('+')}>
+          <button className={operator === '+' ? 'active' : ''} onClick={() => performOperation('+')}>
             +
           </button>
         </div>
         <div className="calc-btn zero">
-          <button onClick={() => addDigit('0')}>0</button>
+          <button onClick={() => inputDigit('0')}>0</button>
         </div>
         <div className="calc-btn">
           <button onClick={() => handleDot()}>.</button>
         </div>
         <div className="calc-btn">
-          <button>=</button>
+          <button onClick={() => performOperation('=')}>=</button>
         </div>
       </div>
     </React.Fragment>
